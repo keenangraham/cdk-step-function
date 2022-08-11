6c3096b4226ad08bfc668bc77eaf7c05c20f7162
Keenan Graham <ksgraham@stanford.edu>
2022-08-10 17:27:53 -0700

    Add state machine

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from aws_cdk import App
 from aws_cdk import Stack
 from aws_cdk import Duration
 
+from aws_cdk.aws_stepfunctions import Pass
+from aws_cdk.aws_stepfunctions import Wait
+from aws_cdk.aws_stepfunctions import WaitTime
+from aws_cdk.aws_stepfunctions import Succeed
+from aws_cdk.aws_stepfunctions import StateMachine
+
 from constructs import Construct
 
 from shared_infrastructure.cherry_lab.environments import US_WEST_2
@@ -10,6 +16,36 @@ from shared_infrastructure.cherry_lab.environments import US_WEST_2
 class StepFunction(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+
+        wait_five_seconds = Wait(
+            self,
+            'WaitFiveSeconds',
+            time=WaitTime.duration(
+                Duration.seconds(5)
+            )
+        )
+
+        no_op = Pass(
+            self,
+            'NoOp',
+        )
+
+        succeed = Succeed(
+            self,
+            'Succeed',
+        )
+
+        definition = wait_five_seconds.next(
+            no_op
+        ).next(
+            succeed
+        )
+
+        state_machine = StateMachine(
+            self,
+            'StateMachine',
+            definition=definition
+        )
 
         
 
