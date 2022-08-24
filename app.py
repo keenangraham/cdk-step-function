81a601178267d1657ae171c2dc16f99a00c35671
Keenan Graham <ksgraham@stanford.edu>
2022-08-23 17:41:38 -0700

    Add

--- app.py
+++ app.py
@@ -8,16 +8,11 @@ from aws_cdk.aws_stepfunctions import WaitTime
 from aws_cdk.aws_stepfunctions import Succeed
 from aws_cdk.aws_stepfunctions import StateMachine
 
+from aws_cdk.aws_stepfunctions_tasks import CallAwsService
+
 from constructs import Construct
 
 from shared_infrastructure.cherry_lab.environments import US_WEST_2
-
-from aws_cdk.pipelines import CodePipeline
-from aws_cdk.pipelines import ShellStep
-from aws_cdk.pipelines import CodePipelineSource
-from aws_cdk import Tags
-
-from aws_cdk.aws_chatbot import SlackChannelConfiguration
 
 
 class StepFunction(Stack):
@@ -42,9 +37,14 @@ class StepFunction(Stack):
             'Succeed',
         )
 
-        definition = wait_five_seconds.next(
-            no_op
-        ).next(
+        list_stacks = CallAwsService(
+            self,
+            'ListStacks',
+            service='cloudformation',
+            action='listStacks',
+        )
+
+        definition = list_stacks.next(
             succeed
         )
 
@@ -52,39 +52,6 @@ class StepFunction(Stack):
             self,
             'StateMachine',
             definition=definition
-        )
-
-        pipeline = CodePipeline(
-            self,
-            'Pipeline',
-            self_mutation=True,
-            synth=ShellStep(
-                'Synth',
-                input=CodePipelineSource.connection(
-                    'keenangraham/cdk-step-function',
-                    'main',
-                    connection_arn='arn:aws:codestar-connections:us-west-2:618537831167:connection/077647ff-f9ef-406e-8392-e092848590f8'
-                ),
-                commands=[
-                    'npm install -g aws-cdk@2.38.1',
-                    'pip install -r requirements.txt -r requirements-dev.txt',
-                    'cdk synth',
-                ]
-            )
-        )
-
-        pipeline.build_pipeline()
-
-        chatbot = SlackChannelConfiguration(
-            self,
-            'encode-dcc-aws-chatbot',
-            slack_channel_configuration_name='aws-chatbot',
-            slack_workspace_id='T1KMV4JJZ',
-            slack_channel_id='C034GTRCCLU',
-        )
-        pipeline.pipeline.notify_on_execution_state_change(
-            'NotifyTest',
-            chatbot
         )
 
 
@@ -96,11 +63,5 @@ step = StepFunction(
     env=US_WEST_2,
 )
 
-Tags.of(step).add(
-    'test',
-    'tag6',
-#    include_resource_types=['AWS::CloudFormation::Stack'],
- #   exclude_resource_types=['AWS::CodeStarNotifications::NotificationRule']
-)
 
 app.synth()
