675426f5b68aa80dd0bf9fb3e827e480a8d1c52e
Keenan Graham <ksgraham@stanford.edu>
2022-08-24 18:52:54 -0700

    Add get stacks to delete logic

--- cdk_step_function/runtime/cloudformation/stacks.py
+++ cdk_step_function/runtime/cloudformation/stacks.py
@@ -1,7 +1,162 @@
-def get_stacks_to_delete(event, context):
-    print(event)
+import boto3
+
+from datetime import datetime
+from datetime import timezone
+
+
+OKAY_STATUSES = [
+    'CREATE_FAILED',
+    'CREATE_COMPLETE',
+    'ROLLBACK_FAILED',
+    'ROLLBACK_COMPLETE',
+    'DELETE_FAILED',
+    'UPDATE_COMPLETE',
+    'UPDATE_FAILED',
+    'UPDATE_ROLLBACK_FAILED',
+    'UPDATE_ROLLBACK_COMPLETE',
+    'IMPORT_COMPLETE',
+    'IMPORT_ROLLBACK_FAILED',
+    'IMPORT_ROLLBACK_COMPLETE'
+]
+
+
+TIME_TO_LIVE_HOURS = 'time-to-live-hours'
+
+SECONDS_IN_AN_HOUR = 3600
+
+
+def get_cloudformation_client():
+    return boto3.client('cloudformation')
+
+
+def get_describe_stacks_paginator(client):
+    return client.get_paginator('describe_stacks')
+
+
+def get_all_stacks(paginator):
     return [
-        'stackA',
-        'stackB',
-        'stackC',
+        stack
+        for result in paginator.paginate()
+        for stack in result['Stacks']
     ]
+
+
+def filter_stacks_by_statuses(stacks):
+    return [
+        stack
+        for stack in stacks
+        if stack['StackStatus'] in OKAY_STATUSES
+    ]
+
+
+def get_tag_by_key(stack, key):
+    for tag in stack['Tags']:
+        if tag['Key'] == key:
+            return tag
+    return None
+
+
+def get_time_to_live_hours_tag_or_none(stack):
+    return get_tag_by_key(stack, TIME_TO_LIVE_HOURS)
+
+
+def stack_has_time_to_live_hours_tag(stack):
+    if get_time_to_live_hours_tag_or_none(stack) is not None:
+        return True
+    return False
+
+
+def filter_stacks_with_time_to_live_hours_tag(stacks):
+    return [
+        stack
+        for stack in stacks
+        if stack_has_time_to_live_hours_tag(stack)
+    ]
+
+
+def get_stack_name(stack):
+    return stack['StackName']
+
+
+def get_creation_time(stack):
+    return stack['CreationTime']
+
+
+def try_parse_time_to_live_hours_tag(tag):
+    try:
+        return int(tag['Value'])
+    except ValueError:
+        print('Tag value not int')
+
+
+def get_current_time():
+    return datetime.now(timezone.utc)
+
+
+def time_to_live_hours_exceeded(creation_time, time_to_live_hours):
+    now = get_current_time()
+    then = creation_time
+    delta = now - then
+    hours_alive = delta.seconds // SECONDS_IN_AN_HOUR
+    return hours_alive >= time_to_live_hours
+
+
+def stack_is_alive_longer_than_time_to_live_hours(stack):
+    creation_time = get_creation_time(
+        stack
+    )
+    time_to_live_hours_tag = get_time_to_live_hours_tag_or_none(
+        stack
+    )
+    time_to_live_hours = try_parse_time_to_live_hours_tag(
+        time_to_live_hours_tag
+    )
+    if time_to_live_hours is not None:
+        return time_to_live_hours_exceeded(
+            creation_time,
+            time_to_live_hours
+        )
+    return False
+
+
+def filter_stacks_living_longer_than_time_to_live_hours(stacks):
+    return [
+        stack
+        for stack in stacks
+        if stack_is_alive_longer_than_time_to_live_hours(stack)
+    ]
+
+
+def get_stacks_to_delete_because_of_time_to_live_hours_tag():
+    stacks = get_all_stacks()
+    stacks = filter_stacks_by_statuses(stacks)
+    stacks = filter_stacks_with_time_to_live_hours_tag(stacks)
+    stacks = filter_stacks_living_longer_than_time_to_live_hours(stacks)
+    return stacks
+
+
+def get_stack_names_from_stacks(stacks):
+    return [
+        get_stack_name(stack)
+        for stack in stacks
+    ]
+
+
+def get_stacks_to_delete(event, context):
+    list_of_lists_of_stacks_to_delete = [
+        get_stacks_to_delete_because_of_time_to_live_hours_tag(),
+        # Extend with other routines here.
+    ]
+    stacks_to_delete = [
+        stack
+        for stack_list in list_of_lists_of_stacks_to_delete
+        for stack in stack_list
+    ]
+    stack_names_to_delete = get_stack_names_from_stacks(
+        stacks_to_delete
+    )
+    unique_stack_names_to_delete = set()
+    unique_stack_names_to_delete.update(
+        stack_names_to_delete
+    )
+    return list(unique_stack_names_to_delete)
