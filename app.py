3a3b14145d2fabc1fdb5388c536bcbfec6cd774c
Keenan Graham <ksgraham@stanford.edu>
2022-08-25 21:56:54 -0700

    Add notification rule and webhook, consumers/producer, and slack messages

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from aws_cdk import Stack
 from aws_cdk import Duration
 from aws_cdk import Tags
 
-from aws_cdk.aws_ssm import StringParameter
-
 from aws_cdk.aws_stepfunctions import Pass
 from aws_cdk.aws_stepfunctions import Wait
 from aws_cdk.aws_stepfunctions import WaitTime
@@ -18,11 +16,14 @@ from aws_cdk.aws_stepfunctions import Choice
 from aws_cdk.aws_stepfunctions import JsonPath
 from aws_cdk.aws_stepfunctions import Condition
 from aws_cdk.aws_stepfunctions import Result
+from aws_cdk.aws_stepfunctions import TaskInput
 
 from aws_cdk.aws_iam import PolicyStatement
 
 from aws_cdk.aws_stepfunctions_tasks import CallAwsService
 from aws_cdk.aws_stepfunctions_tasks import LambdaInvoke
+from aws_cdk.aws_stepfunctions_tasks import EventBridgePutEvents
+from aws_cdk.aws_stepfunctions_tasks import EventBridgePutEventsEntry
 
 from aws_cdk.aws_lambda_python_alpha import PythonFunction
 from aws_cdk.aws_lambda import Runtime
@@ -33,6 +34,80 @@ from shared_infrastructure.cherry_lab.environments import US_WEST_2
 
 from aws_cdk.aws_logs import LogGroup
 from aws_cdk import RemovalPolicy
+
+from aws_cdk import SecretValue
+
+from constructs import Construct
+
+from aws_cdk.aws_events import Rule
+from aws_cdk.aws_events import RuleTargetInput
+from aws_cdk.aws_events import EventPattern
+from aws_cdk.aws_events import Connection
+from aws_cdk.aws_events import Authorization
+from aws_cdk.aws_events import ApiDestination
+
+from aws_cdk.aws_events_targets import ApiDestination as ApiDestinationToTarget
+
+from aws_cdk.aws_ssm import StringParameter
+
+from dataclasses import dataclass
+
+from typing import Any
+
+
+class SlackWebhook(Construct):
+
+    def __init__(
+            self,
+            scope: Construct,
+            construct_id: str,
+            **kwargs: Any
+    ) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+        # Connection needs authorization but this isn't actually used.
+        authorization = Authorization.basic(
+            'abc',
+            SecretValue.unsafe_plain_text('123'),
+        )
+        connection = Connection(
+            self,
+            'Connection',
+            authorization=authorization,
+        )
+        # Reference existing SSM parameter with secret URL.
+        endpoint = StringParameter.from_string_parameter_name(
+            self,
+            'SlackWebhookUrl',
+            string_parameter_name='DEMO_EVENTS_SLACK_WEBHOOK_URL'
+        )
+        api_destination = ApiDestination(
+            self,
+            'SlackIncomingWebhookDestination',
+            connection=connection,
+            endpoint=endpoint.string_value,
+        )
+        # $.detail.data.slack value from event is posted to Slack webhook if
+        # $.detail.metadata.includes_slack_notification is True.
+        target = ApiDestinationToTarget(
+            api_destination=api_destination,
+            event=RuleTargetInput.from_event_path(
+                '$.detail.data.slack'
+            )
+        )
+        rule = Rule(
+            self,
+            'PassEventsToSlack',
+            event_pattern=EventPattern(
+                detail={
+                    'metadata': {
+                        'includes_slack_notification': [True]
+                    }
+                }
+            ),
+            targets=[
+                target
+            ]
+        )
 
 
 
@@ -61,6 +136,11 @@ class StepFunction(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
+        slackhook = SlackWebhook(
+            self,
+            'SlackWebHook'
+        )
+
         log_group = LogGroup(
             self,
             'LogGroup',
@@ -72,6 +152,63 @@ class StepFunction(Stack):
         succeed = Succeed(
             self,
             'Succeed',
+        )
+
+        make_success_message = Pass(
+            self,
+            'MakeSuccessMessage',
+            parameters={
+                'detailType': 'StackDeleteCompleted',
+                'source': 'demo.cleaner',
+                'detail': {
+                    'metadata': {
+                        'includes_slack_notification': True
+                    },
+                    'data': {
+                        'slack': {
+                            'text': JsonPath.format(
+                                ':white_check_mark: *StackDeleteSucceeded* | {}',
+                                JsonPath.string_at('$.stack_to_delete')
+                            )
+                        }
+                    }
+                }
+            },
+        )
+
+        make_failure_message = Pass(
+            self,
+            'MakeFailureMessage',
+            parameters={
+                'detailType': 'StackDeleteFailed',
+                'source': 'demo.cleaner',
+                'detail': {
+                    'metadata': {
+                        'includes_slack_notification': True
+                    },
+                    'data': {
+                        'slack': {
+                            'text': JsonPath.format(
+                                ':x: *StackDeleteFailed* | {}',
+                                JsonPath.string_at('$.stack_to_delete')
+                            )
+                        }
+                    }
+                }
+            },
+        )
+
+        send_slack_notification = EventBridgePutEvents(
+            self,
+            'SendSlackNotiication',
+            entries=[
+                EventBridgePutEventsEntry(
+                    detail_type=JsonPath.string_at('$.detailType'),
+                    detail=TaskInput.from_json_path_at('$.detail'),
+                    source=JsonPath.string_at('$.source')
+                )
+            ],
+            result_path=JsonPath.DISCARD,
         )
 
         get_stacks_to_delete_lambda = PythonFunction(
@@ -91,14 +228,26 @@ class StepFunction(Stack):
             )
         )
 
-        delete_sucessful = Succeed(
+        delete_successful = Pass(
             self,
             'DeleteSuccessful'
+        )
+
+        delete_successful_routine = delete_successful.next(
+            make_success_message
+        ).next(
+            send_slack_notification
         )
 
         unable_to_delete = Pass(
             self,
             'UnableToDelete'
+        )
+
+        unable_to_delete_routine = unable_to_delete.next(
+            make_failure_message
+        ).next(
+            send_slack_notification
         )
 
         increment_counter_lambda = PythonFunction(
@@ -160,7 +309,7 @@ class StepFunction(Stack):
             ),
             increment_counter
         ).otherwise(
-            unable_to_delete
+            unable_to_delete_routine
         )
 
         does_stack_exist = CallAwsService(
@@ -176,10 +325,11 @@ class StepFunction(Stack):
         )
 
         does_stack_exist.add_catch(
-            delete_sucessful,
+            delete_successful_routine,
             errors=[
                 'CloudFormation.CloudFormationException'
-            ]
+            ],
+            result_path='$.errors',
         )
 
         delete_stack = CallAwsService(
@@ -195,10 +345,11 @@ class StepFunction(Stack):
         )
 
         delete_stack.add_catch(
-            unable_to_delete,
+            unable_to_delete_routine,
             errors=[
                 'CloudFormation.CloudFormationException'
-            ]
+            ],
+            result_path='$.errors',
         )
 
         clean_up_routine = increment_counter.next(
@@ -263,6 +414,7 @@ consumer = ConsumerToDelete(
     'ConsumerToDelete2',
     producer=producer,
     env=US_WEST_2,
+    termination_protection=True,
 )
 
 
